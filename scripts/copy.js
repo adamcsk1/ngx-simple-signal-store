2ca2645d9f65a18d62747f6a91032f2ae530d556
adamcsk1 <52232363+adamcsk1@users.noreply.github.com>
2024-08-19 10:52:14 +0200

    chore(license): add license file

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,3 +1,4 @@
 const { copyFileSync } = require('fs');
 
 copyFileSync('./README.md', './dist/ngx-simple-signal-store/README.md');
+copyFileSync('./LICENSE', './dist/ngx-simple-signal-store/LICENSE');
